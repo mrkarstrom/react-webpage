f2d693294ddc37a6b04ffcb19dd2f87a5cd75a09
Magnus Karström <mrkarstrom81@gmail.com>
2024-01-21 08:05:51 +0100

    feat (Button2): Added a Button syled using const.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Card from './Card';
-import Button from "./Button/Button";
+import Button from './Button/Button';
+import Button2 from './Button2';
 
 function App() {
-
-  return(
+  return (
     <>
-    <Card/>
-    <Card/>
-    <Button></Button>
+      <Card />
+      <Card />
+      <Button></Button>
+      <Button2 />
     </>
   );
 }
 
-export default App
+export default App;
---
+++ src/Button2.jsx
@@ -0,0 +1,13 @@
+function Button2() {
+  const styles2 = {
+    backgroundColor: 'hsl(200, 100%, 50%)',
+    color: '#fff',
+    padding: '10px 20px',
+    borderRadius: '5px',
+    border: 'none',
+    cursor: 'pointer',
+  };
+  return <button style={styles2}>Click Me2</button>;
+}
+
+export default Button2;

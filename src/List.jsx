769804f41bb6251e036735895b2f4cf4349f48f6
Magnus Karström <magnus@yesweb.se>
2024-01-28 01:05:02 +0100

    add (List.jsx): Added more properties to the fruits and using id as key.

--- src/List.jsx
+++ src/List.jsx
@@ -1,9 +1,21 @@
 import PropTypes from 'prop-types';
 
 function List() {
-  const fruits = ['apple', 'orange', 'banana', 'coconut', 'pineapple'];
+  const fruits = [
+    { id: 1, name: 'apple', calories: 95 },
+    { id: 2, name: 'orange', calories: 45 },
+    { id: 3, name: 'banana', calories: 105 },
+    { id: 4, name: 'coconut', calories: 159 },
+    { id: 5, name: 'pineapple', calories: 37 },
+  ];
 
-  const listItems = fruits.map((fruit) => <li>{fruit}</li>);
+  fruits.sort();
+
+  const listItems = fruits.map((fruit) => (
+    <li key={fruit.id}>
+      {fruit.name} : {fruit.calories}
+    </li>
+  ));
 
   return <ul>{listItems}</ul>;
 }

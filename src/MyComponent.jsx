9f4d0bb506ce2493d32076d7e0c640ccb2daa807
Magnus Karström <mrkarstrom81@gmail.com>
2024-01-31 04:47:19 +0100

    add (MyComponent.jsx): Added a reactive state function

--- src/MyComponent.jsx
+++ src/MyComponent.jsx
@@ -1,17 +1,31 @@
 import { useState } from 'react';
 
 function MyComponent() {
-  const [name, setName] = useState();
+  const [name, setName] = useState('Guest');
+  const [age, setAge] = useState(99);
 
   const updateName = () => {
     setName('Spongebob');
-    console.log(name);
+  };
+
+  const incAge = () => {
+    if (age < 100) {
+      setAge(age + 1);
+    }
+  };
+  const decAge = () => {
+    if (age > 0) {
+      setAge(age - 1);
+    }
   };
 
   return (
-    <div>
+    <div className="card">
       <p>Name: {name}</p>
       <button onClick={updateName}>Set Name</button>
+      <p>Age: {age}</p>
+      <button onClick={incAge}>^</button>
+      <button onClick={decAge}>v</button>
     </div>
   );
 }

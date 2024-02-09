9ec1a465d25c26bf3217e2b1c4bcdec590ef7a31
Magnus Karström <mrkarstrom81@gmail.com>
2024-02-09 21:55:15 +0100

    Dark mode switch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import React, { useState } from 'react';
 import MyComponent from './MyComponent';
 import PickCar from './PickCar';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleMode = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
 
   return (
-    <div className='card'>
-        <PickCar/>
-        <MyComponent>
-
-        </MyComponent>
-    </div>
+    <>
+      <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+        <h1>{isDarkMode ? 'Dark Mode' : 'Light Mode'}</h1>
+        <button onClick={toggleMode}>
+          {isDarkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+        <div className="card">
+          <PickCar />
+          <MyComponent></MyComponent>
+        </div>
+        </button>
+      </div>
+    </>
   );
 }
 
